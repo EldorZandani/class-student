afb8cc828c6b32a4c68a5d147e7d5d5babee6c18
Eldor <Eldor@ELDOR-PC>
2022-03-08 20:07:15 +0200

    Add project files.

---
+++ class student/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_student
+{
+    internal class Program
+    {
+        private static string name;
+
+        static void Main(string[] args)
+        {
+            int a,b,c,d,e;
+            for(int i = 0; i < 4; i++)
+            Console.WriteLine("please enter student's name");
+            string name = Console.ReadLine();
+            Console.WriteLine("please enter the student's ID");
+            int ID = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter 5 grades of the student's");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+            int grade = (a + b + c + d + e) / 5;
+        }
+    }
+}
---
+++ class student/studet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_student
+{
+    internal class student
+    {
+        string name;
+        int ID;
+        int grade;
+        public student (string name, int ID, int grade) 
+        {
+            this.name = name;
+            this.ID = ID;
+            this.grade = grade;
+        }
+    }
+}
